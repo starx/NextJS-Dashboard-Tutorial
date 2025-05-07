15fb86699acce114f1c8cdba8352723a9be2ad94
starx <mail@mrnepal.com>
2025-05-07 16:01:41 +0100

    insert invoice to db

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { custom, z } from 'zod';
+import postgres from 'postgres';
+ 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
@@ -14,14 +17,11 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true })
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData));
-    console.log(customerId, amount, status);
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    };
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    // Test it out:
-    // console.log(formData, Object.fromEntries(formData), rawFormData);
+    
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
 }
