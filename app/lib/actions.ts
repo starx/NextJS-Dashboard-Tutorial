88fa125ecff37c7a165541255454abe88fba5b01
starx <mail@mrnepal.com>
2025-05-08 14:04:23 +0100

    fixing lint error by using log

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -57,6 +57,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch(error) {
+        console.log(error);
         // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Create Invoice.',
