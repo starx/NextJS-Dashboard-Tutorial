3420f9f566c13d7801f1881bdf7eac7673f8e43b
starx <mail@mrnepal.com>
2025-05-10 01:35:50 +0100

    ensure type exists during compile time

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -3,6 +3,10 @@
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
+function isAuthErrorWithType(error: unknown): error is { type: string } {
+  return typeof error === 'object' && error !== null && 'type' in error;
+}
+
 export async function authenticate(
     prevState: string | undefined,
     formData: FormData,
@@ -10,7 +14,7 @@ export async function authenticate(
     try {
       await signIn('credentials', formData);
     } catch (error) {
-      if (error instanceof AuthError) {
+      if (error instanceof AuthError && isAuthErrorWithType(error)) {
         switch (error.type) {
           case 'CredentialsSignin':
             return 'Invalid credentials.';
