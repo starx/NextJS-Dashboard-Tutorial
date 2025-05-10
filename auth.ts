b0fb679c8c8407068d4c41340200d4b0baacf1e1
starx <mail@mrnepal.com>
2025-05-10 01:36:41 +0100

    correct type

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
-import type { User } from '@/lib/definitions';
+import type { User } from './lib/types/user';
 import bcrypt from 'bcryptjs';
 import { sql } from '@/lib/db';
 
